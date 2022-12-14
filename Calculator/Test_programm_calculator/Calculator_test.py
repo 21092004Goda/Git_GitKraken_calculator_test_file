9dac6105b87faf8d35fca6f1bee536a1f29478e0
Kuro <96621375+21092004Goda@users.noreply.github.com>
2022-12-14 06:20:18 +0300

    Update Calculator_test.py

--- Calculator/Test_programm_calculator/Calculator_test.py
+++ Calculator/Test_programm_calculator/Calculator_test.py
@@ -1,5 +1,24 @@
 from unittest import TestCase, main
-from calculator_in.calculator import calculator
+
+def calculator(function):
+    symbol = '+-/*'
+    if not any(sign in function for sign in symbol):
+        raise ValueError(f'Некорректно выражение, нет допустимых знаков ({symbol})')
+    for sign in symbol:
+        if sign in function:
+            try:
+                left, right = function.split(sign)
+                left, right = int(left), int(right)
+                return {
+                    '+': lambda a, b: a + b,
+                    '*': lambda a, b: a * b,
+                    '/': lambda a, b: a / b,
+                    '-': lambda a, b: a - b,
+                }[sign](left, right)
+            except (ValueError, TypeError):
+                raise ValueError('Выражение должно содержать целые числа и один знак')
+            except(ZeroDivisionError):
+                raise ZeroDivisionError('На ноль не делят')
 
 
 class CalculatorTest(TestCase):
